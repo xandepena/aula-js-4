8fb82c259eb19a2c3d1e814e9c378c5e329dac48
xandepena <161486267+xandepena@users.noreply.github.com>
2024-05-21 11:54:24 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,34 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
   const taskList = document.getElementById('task-list');
 
   const addTask = () => {
-    const taskText = newTaskInput.value.trim(); 
-    if (taskText != '') {
+    const taskText = newTaskInput.value.trim()
+    if (TaskText != ''){
       const li = document.createElement('li');
-      li.innerHTML = `
-      <span>${taskText}</span>
-      <button class="delete-btn">Delete</button>
-      `;
-
-      taskList.appendChild(li);
+      li.innerHTML = `<span>${taskText}</span> <button class="delete-btn">Delete</button>`;
+      
+      taskList.appendChild(li)
 
       li.querySelector('.delete-btn').addEventListener('click', () => {
-        li.remove();
-      });
+        li.remove()
+      })
 
       li.addEventListener('click', () => {
-        li.classList.toggle('completed');
-      });
+        li.classList.toggle('completed')
+      })
 
       newTaskInput.value = '';
-    } 
-  };
 
-  addTaskBtn.addEventListener('click', addTask);
+
+    }
+  }
+
+  addTaskBtn.addEventListener('click', addTask)
+
 
   newTaskInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
-      addTask();
+      addTask()
     }
-  });
-});
-
+  })
+})
